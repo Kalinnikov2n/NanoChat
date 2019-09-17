e2113793bd1361227390dd2eed35991776b97da0
Nikita Kalinnikov <nikitakalinnikov@KALL.local>
2019-09-18 02:24:22 +0300

    схема стэйта

--- src/components/ChatWindow.js
+++ src/components/ChatWindow.js
@@ -5,6 +5,15 @@ const regeneratorRuntime =  require("regenerator-runtime");
 
 
 class ChatWindow extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          uuid:"", 
+          cuid: '', 
+          history: [{}],
+          inputMesg: '',
+        }
+      }
     render(){
         return(
             <div>
