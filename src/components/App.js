c951e3e9c28ab3f5238db1106867a845dea2a232
Nikita Kalinnikov <nikitakalinnikov@KALL.local>
2019-09-17 21:50:58 +0300

    Подтянул скелет вебпак + галп + реакт

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+import '../public/styles/index.css';
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Я  ШОКЕ!</h1>
+            </div>
+        );
+    }
+}
+
+export default App;
