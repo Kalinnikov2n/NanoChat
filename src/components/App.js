5714e8a5e9d7c8bb3f0921f5435446bf1b8339c0
Никита Калинников <kalinnikov@nanosemantics.ai>
2020-09-02 15:43:04 +0300

    inti

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,15 @@
 import React, { Component } from "react";
-import '../public/styles/index.css';
+import "../public/styles/index.css";
 import Chat from "./Chat";
 
 class App extends Component {
-    render() {
-        return (
-            <div className= "App">
-                <Chat/>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="App">
+        <Chat />
+      </div>
+    );
+  }
 }
 
 export default App;
