3843684dbdb8765e77450b9e1c37f4b1abea7c9a
Никита Калинников <kalinnikov2n@yandex.com>
2020-09-02 15:58:12 +0300

    hhhh

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ import Chat from "./Chat";
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className=" App">
         <Chat />
       </div>
     );
